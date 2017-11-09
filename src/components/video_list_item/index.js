6898a02fc8f1af3d9733af753bffb2e90b6ab3f7
Luis Manzo <manzosk3@gmail.com>
2017-11-09 15:32:50 -0800

    uses bootstrap media classes to show thumbnail and title of video, inside VideoListItem component

--- src/components/video_list_item/index.js
+++ src/components/video_list_item/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const VideoListItem = props => {
-  return <li>Video</li>
+const VideoListItem = ({ video }) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <img classame="media-object" src={imageUrl} />
+      </div>
+
+      <div clasName="media-body">
+        <div className="media-heading">{video.snippet.title}</div>
+      </div>
+    </li>
+
+  );
 }
 
 export default VideoListItem;
