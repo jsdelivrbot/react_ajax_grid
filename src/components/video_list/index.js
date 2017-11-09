3a637ff337a3c2017ef38ccfaa01e46ee4e4121f
Luis Manzo <manzosk3@gmail.com>
2017-11-09 14:09:17 -0800

    inside VideoList component, renders a list of VideoListItems, etag is unique key

--- src/components/video_list/index.js
+++ src/components/video_list/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import VideoListItem from './../video_list_item/index.js';
 
 const VideoList = function(props) {
+
+  const videoItems = props.videos.map((video, i) => {
+    return <VideoListItem key={video.etag} video={video}/>
+  });
+
   return (
     <ul className='col-md-4 list-group'>
-      {props.videos.length}
+      {videoItems}
     </ul>
   )
 }
