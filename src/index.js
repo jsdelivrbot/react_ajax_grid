d33cfeb35dc39c63991600a91ca0c93b20bb755e
Luis Manzo <manzosk3@gmail.com>
2017-11-09 09:48:37 -0800

    makes ajax query to yt api, logs to console

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+let API_KEY = 'AIzaSyB3nRBM5KuOYW9q94ZVOd8redUcGSuAFKY';
+
+YTSearch({key: API_KEY, term: 'jordan peterson'}, data => {
+  console.dir(data);
+})
 
 class App extends React.Component {
    render() {
